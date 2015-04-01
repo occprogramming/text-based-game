7f4a201d9931ee8cf898278fb306557af1f36a13
Georgy Marrero <geoxxstyle@gmail.com>
2015-03-31 22:13:46 -0700

    Create stock Bully class

---
+++ Bully.cpp
@@ -0,0 +1,9 @@
+//
+//  Bully.cpp
+//  
+//
+//  Created by Georgy Marrero on 3/31/15.
+//
+//
+
+#include "Bully.h"
---
+++ Bully.h
@@ -0,0 +1,14 @@
+//
+//  Bully.h
+//  
+//
+//  Created by Georgy Marrero on 3/31/15.
+//
+//
+
+#ifndef ____Bully__
+#define ____Bully__
+
+#include <stdio.h>
+
+#endif /* defined(____Bully__) */
