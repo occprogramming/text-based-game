50b168938cec943313a4f78bb0970eb1eee632c9
Georgy Marrero <geoxxstyle@gmail.com>
2015-04-17 12:41:25 -0700

    Fix dialogue header and implementation

---
+++ dialogue.cpp
@@ -0,0 +1,62 @@
+#ifndef DIALOGUE_CPP
+#define DIALOGUE_CPP
+
+
+#include "dialogue.h"
+
+// Gameplay is expressed using dialogues, which present a piece of
+// information and some responses, and the ask the user to pick one. If
+// they do not pick a valid one then the dialogue loops until they do
+
+
+	// Initial piece of information that the dialogue displays
+	//std::string description;
+
+	// A vector of choices that will be outputted. No numbering is
+	// necessary, the dialogue does that automatically
+	//std::vector<std::string> choices;
+
+	// Run the dialogue
+
+Dialogue::Dialogue() {}
+
+
+Dialogue::Dialogue(const string& setUp, const string& question, const vector<string>& choices)
+{
+    this->setUp = setUp;
+    this->question = question;
+    this->choices = choices;
+}
+
+
+int Dialogue::activate()
+{
+    // Output the information
+    cout << description << endl;
+
+    // Output and number the choices
+    for(int i = 0; i < this->choices.size(); ++i)
+    {
+        cout << i+1 << ": " << this->choices[i] << endl;
+    }
+
+    int userInput = -1;
+
+    // Repeatedly read input from stdin until a valid option is
+    // chosen
+    while(true)
+    {
+        cin >> userInput;
+        // 'Valid' means within the range of numbers outputted
+        if(userInput >= 0 && userInput <= choices.size())
+        {
+            return userInput;
+        }
+    }
+
+    return 0;
+}
+
+
+
+#endif /* DIALOGUE_CPP */
--- dialogue.h
+++ dialogue.h
@@ -6,6 +6,7 @@
 #include <iostream>
 
 using namespace std;
+
 // Gameplay is expressed using dialogues, which present a piece of
 // information and some responses, and the ask the user to pick one. If
 // they do not pick a valid one then the dialogue loops until they do
@@ -15,15 +16,21 @@ class Dialogue
 public:
 	// Default constructor
 	Dialogue();
+    
 	// Overloaded constructor
-	Dialogue(string description, vector<string> choices);
+	Dialogue::Dialogue(const string& setUp, const string& question, const vector<string>& choices)
+    
 	// Run the dialogue
 	int activate();
 
 private:
 
 	// Initial piece of information that the dialogue displays
-	string description;
+	string setUp;
+    
+    // Question
+    string question;
+    
 	// A vector of choices that will be outputted. No numbering is
 	// necessary, the dialogue does that automatically
 	vector<string> choices;
