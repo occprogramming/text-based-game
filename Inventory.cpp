293c247eeac0fb14a0936348dea1c89e839e14e5
Chuong Vu <chuongv@gmail.com>
2015-05-01 12:38:36 -0700

    Update inventory delete method

--- Inventory.cpp
+++ Inventory.cpp
@@ -34,6 +34,7 @@ void Inventory::add_item(Item* item, int count)
             it->second += count;
             found = true;
         }
+        it++;
     }
 
     // If the item doesn't already exist in the inventory, then a
@@ -41,31 +42,31 @@ void Inventory::add_item(Item* item, int count)
     if (!found)
         items.push_back(make_pair(item, count));
 }
-//
-//// Removes an item from the inventory.
-//void Inventory::remove_item(Item* item, int count)
-//{
-//    // Iterate through the items, and if they are found then decrease
-//    // the quantity by the quantity removed
-//    for(Item& it : this->items)
-//    {
-//        if(it.first == item)
-//        {
-//            it.second -= count;
-//            return;
-//        }
-//    }
-//    
-//    // Iterate through the list again, and remove any elements from
-//    // the list that have zero or less for their quantity
-//    // We do this in two passes because removing an element from
-//    // a list during a for loop invalidates the iterators, and the
-//    // loop stops working
-//    items.remove_if([](pair<Item*, int>& element)
-//    {
-//                              return element.second < 1;
-//    });
-//}
+
+// Removes an item from the inventory.
+void Inventory::remove_item(Item* item, int count)
+{
+    // Iterate through the items, and if they are found then decrease
+    // the quantity by the quantity removed
+    list<pair<Item*, int>>::iterator it = items.begin();
+    while (it != items.cend()) {
+        if (it->first == item)
+        {
+            // Once we find the item, we update the quantity.
+            it->second -= count;
+        }
+        it++;
+    }
+    // Iterate through the list again, and remove any elements from
+    // the list that have zero or less for their quantity
+    // We do this in two passes because removing an element from
+    // a list during a for loop invalidates the iterators, and the
+    // loop stops working
+    items.remove_if([](pair<Item*, int>& element)
+    {
+                              return element.second < 1;
+    });
+}
 //
 //// Adds all items from another inventory.
 //void Inventory::merge(Inventory* inventory)
